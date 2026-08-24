8b46a577fdb1199211659d5bb535f6de7765e5bd
leung372 <chingheikelvin.leung@mail.utoronto.ca>
2026-08-23 23:59:33 -0400

    remove miniconda installer and update gitignore

--- Baselines/GMDNet/algorithm/gmdnet/train.py
+++ Baselines/GMDNet/algorithm/gmdnet/train.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
 from my_utils.utils import *
 import numpy as np
 from torch.utils.data import Dataset
